1cf442d07e797aca5ef40e363e221388bbcdcefa
Lucky <luckykashyap3430@gmail.com>
2023-09-28 19:49:41 +0530

    declare with var

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // start with js interview questions
 
+// start with var vs let vs const
+// we can declare variables using these 3 keywords
 
+
+console.log(num);
+var num = 10;
+
+console.log(num);
