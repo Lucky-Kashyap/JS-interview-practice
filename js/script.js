5d418b9fc614c7fa449b6983f3dbc66146c5ae79
Lucky <luckykashyap3430@gmail.com>
2023-09-28 19:53:29 +0530

    about scope

--- js/script.js
+++ js/script.js
@@ -4,7 +4,24 @@
 // we can declare variables using these 3 keywords
 
 
-console.log(num);
-var num = 10;
+// console.log(num);
+// var num = 10;
+
+// console.log(num);
+
+// scope
+
+// it is a certain region where particular variable can be accessed beyond that we can not access
+
+// var is functional scope but let & const is block scope
+
+
+var a = 5;
+console.log(a);
 
 console.log(num);
+
+let num = 20;
+
+console.log(num);
+
