6ca4a19200df14c8297b46ef65d7ef4bfb154447
Lucky <luckykashyap3430@gmail.com>
2023-10-14 22:59:50 +0530

    for loop interview question on block scope var vs let in setTimeout

--- js/forloop.js
+++ js/forloop.js
@@ -4,8 +4,26 @@
 //     console.log(i);
 // }
 
-for(let i=0;i<3;i++){
-    setTimeout(()=>{
-        console.log(i);
-    },1000);
-}
+// for(var i=0;i<3;i++){
+//     console.log(i);
+// }
+
+// for(let i=0;i<3;i++){
+//     setTimeout(()=>{
+//         console.log(i);
+//     },1000);
+// }
+
+// for (var i = 0; i <=3; i++) {
+//   setTimeout(() => {
+//     console.log(i + i);
+//   }, 1000);
+// }
+
+
+for (let i = 0; i <=3; i++) {
+    setTimeout(() => {
+      console.log(i + i);
+    }, 1000);
+  }
+  
