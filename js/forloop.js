92bdd8c12762f3c388b3d34e76d60bd3453afc37
Lucky <luckykashyap3430@gmail.com>
2023-10-15 12:59:25 +0530

    for loop deep understanding interview question

--- js/forloop.js
+++ js/forloop.js
@@ -20,10 +20,104 @@
 //   }, 1000);
 // }
 
+// for (let i = 0; i <=3; i++) {
+//     setTimeout(() => {
+//       console.log(i + i);
+//       console.log(i);
+//     }, 1000);
+//   }
 
-for (let i = 0; i <=3; i++) {
-    setTimeout(() => {
-      console.log(i + i);
-    }, 1000);
-  }
-  
+// for (let i = 0; i <= 3; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+
+// for (let i = 0; i <= 3; i++)
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+
+// if(true){
+//   let a = 5;
+// }
+
+// forloop.js:47 Uncaught SyntaxError: Lexical declaration cannot appear in a single-statement context (at forloop.js:47:5)
+
+// if(true)
+//   let a = 5;
+
+// if(true)
+//     var a = 5;
+
+// for (;;) {
+//   console.log("hi");
+// }
+
+// for(let i=1;i<=3;i++){
+
+//   const callback=()=>{
+//     console.log(i);
+//   }
+
+//   setTimeout(callback,1000);
+// }
+
+// const arr=[];
+
+// for(let i=1;i<=3;i++){
+
+//   const callback=()=>{
+//     console.log(i);
+//   }
+
+//   arr.push(callback);
+//   setTimeout(callback,1000);
+// }
+
+// const arr=[];
+
+// for(let i=1;i<=3;i++){
+
+// const callback=()=>{
+// console.log(i);
+// }
+
+// console.log(callback);
+// console.dir(callback);
+
+// arr.push(callback);
+// setTimeout(callback,1000);
+// }
+
+// more examples
+
+// for(let i=0;getI=()=>i, i<3;i++){
+//   console.log(getI());
+// }
+
+// const arr=[];
+
+// for(let i=0,getI=()=>i; i<3;i++){
+//   arr.push(getI);
+//   // console.dir(getI);
+//   console.log(getI());
+// }
+
+// const arr=[];
+// // 0 1 2
+// for(let i=0,getI=()=>i; i<3;i++,getI=()=> i){
+//   arr.push(getI);
+//   // console.dir(getI);
+//   console.log(getI());
+// }
+
+// alg alg baar function create hoga
+
+for (
+  let i = 0, getI = () => i, incrementI = () => i++;
+  getI() < 3;
+  incrementI()
+) {
+  console.log(i);
+}
