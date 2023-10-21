75786fc5ba94152ce415fffcb6d6592de0e64d37
Lucky <luckykashyap3430@gmail.com>
2023-10-21 21:33:35 +0530

    objects output based questions

---
+++ js/objects.js
@@ -0,0 +1,34 @@
+// About objects
+
+// const user = {
+//     name:'Lucky',
+//     age:23,
+// }
+
+// user.name = 'Divyanshu';
+
+// delete user.age;
+
+// console.log(user);
+
+// console.log(user.name);
+
+// delete is used in case of objects not local variables
+
+
+// const func = (function(a){
+//     delete a;
+//     return a;
+// })(5);
+
+// console.log(func);   // 5
+
+
+const user= {
+    name:'Lucky',
+    age:23,
+    "Like this repo":true,
+}
+
+
+console.log(user);
