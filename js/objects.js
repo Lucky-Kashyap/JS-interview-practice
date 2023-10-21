608e2ce0cb240d2c8f6742e50b634208801672ef
Lucky <luckykashyap3430@gmail.com>
2023-10-21 22:07:05 +0530

    undertsanding JSON parse & stringify

--- js/objects.js
+++ js/objects.js
@@ -78,24 +78,67 @@
 
 //  create a fucntion multiplyByTwo(obj) that multiplies all numeric property values of nums by 2
 
-let nums = {
-  a: 100,
-  b: 200,
-  title: "My nums",
-};
+// let nums = {
+//   a: 100,
+//   b: 200,
+//   title: "My nums",
+// };
 
 // console.log(nums);
 
-function multiplyByTwo(nums) {
-  for (key in nums) {
-    if (typeof nums[key] === "number") {
-      console.log(nums[key] * 2);
-    }
-  }
-}
+// function multiplyByTwo(nums) {
+//   for (key in nums) {
+//     if (typeof nums[key] === "number") {
+//       console.log(nums[key] * 2);
+//     }
+//   }
+// }
 
 // function multiplyByTwo(nums){
 //     console.log(nums*2);
 // }
 
-multiplyByTwo(nums);
+// console.log(nums);
+// multiplyByTwo(nums);
+
+// what is output
+
+// const a = {};
+
+// const b = {key:'b'};
+// const c = {key:"c"};
+
+// a[b] = 123;
+// a[c] = 456;   // Object Object
+
+// console.log(a[b]);
+
+// what is JSON.stringify & JSON.parse  ?
+
+const user = {
+  name: "lucky",
+  age: 24,
+};
+
+// const strObj = JSON.stringify(user);
+
+// localStorage.setItem('test',user);
+// localStorage.setItem('test',strObj);   // store obj into local storage
+
+// console.log(JSON.parse(localStorage.getItem('test')));
+
+
+// console.log(strObj);
+
+// console.log(strObj.name);
+
+// const convertObj = JSON.parse(strObj);
+
+// console.log(convertObj);
+
+// console.log(JSON.parse(strObj));
+
+// console.log(user.name);
+
+// console.log(user);
+// console.log(JSON.stringify(user));
