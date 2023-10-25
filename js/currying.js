e8e02bd7b47c822d8b2337fa9886e03900da72c2
Lucky <luckykashyap3430@gmail.com>
2023-10-25 22:53:51 +0530

    evaluate different functions with currying using condition

--- js/currying.js
+++ js/currying.js
@@ -26,12 +26,35 @@
 
 // sum(2)(6)(1)
 
-function sum(a) {
-  return function (b) {
-    return function (c) {
-      return a + b + c;
+// function sum(a) {
+//   return function (b) {
+//     return function (c) {
+//       return a + b + c;
+//     };
+//   };
+// }
+
+// console.log(sum(2)(6)(1));
+
+// evaluate('sum')(4)(2) => 6
+// evaluate('multiply')(4)(2) => 8
+// evaluate('divide')(4)(2) => 2
+// evaluate('subtract')(4)(2) => 2
+
+function evaluate(op) {
+  return function (a) {
+    return function (b) {
+      if (op === "sum") return a + b;
+      else if (op === "multiply") return a * b;
+      else if (op === "divide") return a / b;
+      else if (op === "substract") return a - b;
+      else return "Invalid operation";
     };
   };
 }
 
-console.log(sum(2)(6)(1));
+console.log(evaluate("sum")(4)(2));
+
+console.log(evaluate("multiply")(4)(2));
+console.log(evaluate("divide")(4)(2));
+console.log(evaluate("substract")(4)(2));
