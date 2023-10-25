bc2c8f03e615aa02e9b0c72f0846ee4a33608b1a
Lucky <luckykashyap3430@gmail.com>
2023-10-25 22:40:51 +0530

    currying in js

---
+++ js/currying.js
@@ -0,0 +1,19 @@
+// currying
+
+// function f(a,b){
+//     console.log(a,b);
+// }
+
+
+// function f(a){
+//     return fn(b){
+//         console.log(a,b);
+//     }
+//     fn(b);
+// }
+
+
+// f(10,20);
+
+// let res = f(10)(20);
+// console.log(res);
