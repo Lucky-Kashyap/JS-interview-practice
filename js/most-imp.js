60780f10b07693b9743ec12cfbe3e02defbe3dbb
Lucky <luckykashyap3430@gmail.com>
2023-10-26 21:21:27 +0530

    var vs let vs const

---
+++ js/most-imp.js
@@ -0,0 +1,65 @@
+// most important
+
+// var vs let vs const
+
+// var a = 10;
+
+// let a = 20;
+
+// var a = 20;
+
+// console.log(a);
+
+// let b = 20;
+
+// console.log(b);
+
+// let block scope const block scope
+// var function scope
+
+// console.log("Hello console")
+
+// var = block level scope and global scope
+// let = block level scope
+// const = block level scope
+
+// var Name = 'suman'
+
+// const print = () =>{
+//   var Name = 'ram';
+// console.log(Name);
+// }
+// print();
+// var Name = 'mohan'
+// console.log(Name)
+
+// function print(){
+// var Name = 'Luckcy';
+
+// console.log(Name);
+// }
+
+// print();
+
+// console.log(Name);
+
+// let Name = 'suman'
+
+// const print = () =>{
+//     let Name = 'ram';
+//     console.log(Name);
+// }
+
+// print();
+// Name = 'mohan'
+// console.log(Name)
+
+// const Name = 'suman'
+
+// const print = () =>{
+//     let Name = 'ram';
+//     console.log(Name);
+// }
+// print();
+// Name = 'mohan'
+// console.log(Name)
