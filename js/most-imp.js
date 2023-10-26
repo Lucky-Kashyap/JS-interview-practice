6c61c99d4ec2daba4bee38e0495a4014fedc6cf8
Lucky <luckykashyap3430@gmail.com>
2023-10-26 22:27:32 +0530

    method chaining promises

--- js/most-imp.js
+++ js/most-imp.js
@@ -263,3 +263,49 @@
 //     fetchDataAPITwo,
 //     fetchDataAPIThree,
 // ]).then((suman)=>console.log(suman))
+
+
+// async await
+// defenition :- Use of async and await enables the use of ordinary try / catch blocks around asynchronous code. Note: The await keyword is only valid inside async functions within regular JavaScript code. If you use it outside of an async function's body, you will get a SyntaxError .
+
+function otpVarify(otp) {
+    return new Promise((resolve, reject) => {
+        console.log('varifying otp...')
+        if (otp === 1234) {
+            resolve('Welcome to Our Website')
+        } else {
+            reject('otp is not vailid')
+        }
+    })
+}
+
+function processRequest(respose) {
+    return new Promise((resolve, reject) => {
+        console.log('Processing Response')
+        resolve(respose)
+    })
+}
+
+// const otp = 12345;
+const otp = 1234;
+
+otpVarify(otp).then(msg => {
+    console.log("OTP received");
+    return processRequest(msg)
+}).then(msg => console.log(msg)).catch(err => console.log(err))
+
+// const otp = 12345
+// const otpVarificationFunction = async () =>{
+//     try {
+//         const respose =  await otpVarify(otp)
+//         console.log("OTP received");
+//         const processrespose =  await processRequest(respose)
+//         console.log(processrespose)
+//     } catch (error) {
+//         console.log(error)
+//     }
+ 
+// }
+
+// otpVarificationFunction();
+
