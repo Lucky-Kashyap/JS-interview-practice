bd6168281d222a56421ae2e42abe220bf9ac38c9
Lucky <luckykashyap3430@gmail.com>
2023-10-27 22:16:59 +0530

    check if an object is array or not

--- js/questions.js
+++ js/questions.js
@@ -1,25 +1,43 @@
 // 1 - Given a string, reverse each word in the sentence
 
-let str = "Harsh bhai kese ho";
+// let str = "Harsh bhai kese ho";
 
 // let res = str.split(' ').map(function(word){
 //     return word.split('').reverse().join('')
 // });
 
+// console.log(res.join(' '));
 // console.log(res);
 
-function reverse(str){
-    let rev = '';
+// function reverse(str){
+//     let rev = '';
 
-    rev = str.split('').reverse().join('');
+//     rev = str.split('').reverse().join('');
 
-    // rev.reverse();
+//     // rev.reverse();
 
-    // rev.join("");
+//     // rev.join("");
 
-    return rev;
+//     return rev;
+// }
+
+// let res = reverse(str);
+
+// console.log(res);
+
+
+
+// 2 - how to check if an object is an array or not
+
+
+function checkArr(el){
+    let check = Array.isArray(el);
+
+    console.log(check);
+
+
 }
 
-let res = reverse(str);
+checkArr([]);
+checkArr({});
 
-console.log(res);
