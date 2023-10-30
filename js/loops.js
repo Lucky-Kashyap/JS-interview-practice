f7a7ba5bfc9937e055b4d6ee830d47a191afdfb9
Lucky <luckykashyap3430@gmail.com>
2023-10-30 21:41:00 +0530

    check palindrome or not

--- js/loops.js
+++ js/loops.js
@@ -41,20 +41,62 @@
 
 // Problem 3: Counting the number of digits
 
-function countNumberOfDigits(digit) {
-  let count = 0;
+// function countNumberOfDigits(digit) {
+//   let count = 0;
 
-  while (digit > 0) {
-    let rem = digit % 10;
-    count++;
-    digit = Math.floor(digit / 10);
+//   while (digit > 0) {
+//     let rem = digit % 10;
+//     count++;
+//     digit = Math.floor(digit / 10);
+//   }
+
+//   return count;
+// }
+
+// function countNumberOfDigits(digit){
+// digit = Math.abs(digit);
+
+//     let count = 0;
+
+//     do{
+//         count++;
+//         digit = Math.floor(digit / 10);
+//     }while(digit > 0);
+
+//     return count++;
+// }
+
+// let digit = 123467;
+// let digit = -121;
+
+// let res = countNumberOfDigits(digit);
+
+// console.log(`Count Digits ${digit} : ${res}`);
+
+// console.log(`Count Digits ${digit} : ${res}`);
+
+// Problem 4: Checking for palindrome numbers
+
+function checkPalindromeNumber(num) {
+  let bool = false;
+
+  let rev = 0;
+  let temp = num;
+  while (temp > 0) {
+    let rem = temp % 10;
+    rev = rem + rev * 10;
+    temp = Math.floor(temp / 10);
   }
 
-  return count;
+  if (rev == num) {
+    bool = true;
+  }
+
+  return bool;
 }
 
-let digit = 123467;
+let num = 1121;
 
-let res = countNumberOfDigits(digit);
+let res = checkPalindromeNumber(num);
 
-console.log(`Count Digits ${digit} : ${res}`);
+console.log(`Number palindrome for  ${num}  : ${res}`);
