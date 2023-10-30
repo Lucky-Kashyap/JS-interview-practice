a04e9fdebde148aaa80e5f1395a1e3a7c75272ba
Lucky <luckykashyap3430@gmail.com>
2023-10-30 20:16:54 +0530

    sum of n natural number

---
+++ js/loops.js
@@ -0,0 +1,19 @@
+// Problem 1: Finding the sum of natural numbers
+
+
+function sumOfNaturalNumbers(num){
+    let sum =0;
+
+    // for(let i=1;i<=num;i++){
+    //     sum += i;
+    // }
+
+    
+    return sum;
+}
+
+let num = 10;
+
+let res = sumOfNaturalNumbers(num);
+
+console.log('Sum of n natural numbers : ' + res);
