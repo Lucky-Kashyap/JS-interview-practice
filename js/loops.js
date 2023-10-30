c1a01f86d3dc3e62a17913f41ace1c4096eb5331
Lucky <luckykashyap3430@gmail.com>
2023-10-30 20:18:08 +0530

    using another way to sum of n natural number

--- js/loops.js
+++ js/loops.js
@@ -1,19 +1,19 @@
 // Problem 1: Finding the sum of natural numbers
 
+function sumOfNaturalNumbers(num) {
+  let sum = 0;
 
-function sumOfNaturalNumbers(num){
-    let sum =0;
+  // for(let i=1;i<=num;i++){
+  //     sum += i;
+  // }
 
-    // for(let i=1;i<=num;i++){
-    //     sum += i;
-    // }
+  sum = (num * (num + 1)) / 2;
 
-    
-    return sum;
+  return sum;
 }
 
-let num = 10;
+let num = 5;
 
 let res = sumOfNaturalNumbers(num);
 
-console.log('Sum of n natural numbers : ' + res);
+console.log("Sum of n natural numbers : " + res);
