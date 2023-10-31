0027c8bfbb2aa685b89a90ad7ae1ed4a8bc6ae6a
Lucky <luckykashyap3430@gmail.com>
2023-10-31 14:29:16 +0530

    factorial of largest number

--- js/string.js
+++ js/string.js
@@ -1,7 +1,4 @@
-
-
 // Question 1: Find the longest word length
-
 
 // function findLongestWordLength(str){
 
@@ -16,46 +13,60 @@
 //     return count;
 // }
 
-
 // const words = ['apple', 'banana', 'cherry', 'dragonfruit', 'elderberry'];
 
 // let res = findLongestWordLength(words);
 
 // console.log(words[3].length);
 
-
 // console.log(`Longest word Length ${res}`);
-
 
 // Question 2 -  Find the longest word
 
+// function findLongestWord(str){
 
+//     let count=str[0].length;
+//     let word ='';
 
-function findLongestWord(str){
+//     for(let i=0;i<str.length;i++){
+//         if(str[i].length > count){
+//             count=str[i].length;
+//             word=str[i];
+//         }
+//     }
 
-    let count=str[0].length;
-    let word ='';
+//     return word;
+// }
 
-    for(let i=0;i<str.length;i++){
-        if(str[i].length > count){
-            count=str[i].length;
-            word=str[i];
-        }
-    }
+// const words = ['apple', 'banana', 'cherry', 'dragonfruit', 'elderberry'];
 
-    return word;
-}
-
-
-const words = ['apple', 'banana', 'cherry', 'dragonfruit', 'elderberry'];
-
-let res = findLongestWord(words);
+// let res = findLongestWord(words);
 
 // console.log(words[3].length);
 
+// console.log(`Longest word Length ${res}`);
 
-console.log(`Longest word Length ${res}`);
+// Question 3: Calculate the factorial of the largest number in the array
 
+function factorialLargest(arr) {
+  let fact = 1;
+  let count = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > count) {
+      count = arr[i];
+    }
+  }
 
+  for (let i = count; i > 0; i--) {
+    fact = fact * i;
+  }
 
+  return fact;
+}
+
+const numbers = [5, 2, 8, 4, 3];
+
+let res = factorialLargest(numbers);
+
+console.log(`Factorial of the largest number : ${res}`);
