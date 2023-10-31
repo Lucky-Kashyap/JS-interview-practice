c24f89917dcee6fd7c2361b1b449a96daa0bc4dd
Lucky <luckykashyap3430@gmail.com>
2023-10-31 13:55:40 +0530

    longest word find

--- js/string.js
+++ js/string.js
@@ -3,25 +3,59 @@
 // Question 1: Find the longest word length
 
 
-function findLongestWordLength(str){
+// function findLongestWordLength(str){
 
-    let count=0;
+//     let count=0;
+
+//     for(let i=0;i<str.length;i++){
+//         if(str[i].length > count){
+//             count=str[i].length;
+//         }
+//     }
+
+//     return count;
+// }
+
+
+// const words = ['apple', 'banana', 'cherry', 'dragonfruit', 'elderberry'];
+
+// let res = findLongestWordLength(words);
+
+// console.log(words[3].length);
+
+
+// console.log(`Longest word Length ${res}`);
+
+
+// Question 2 -  Find the longest word
+
+
+
+function findLongestWord(str){
+
+    let count=str[0].length;
+    let word ='';
 
     for(let i=0;i<str.length;i++){
         if(str[i].length > count){
             count=str[i].length;
+            word=str[i];
         }
     }
 
-    return count;
+    return word;
 }
 
 
 const words = ['apple', 'banana', 'cherry', 'dragonfruit', 'elderberry'];
 
-let res = findLongestWordLength(words);
+let res = findLongestWord(words);
 
-console.log(words[3].length);
+// console.log(words[3].length);
 
 
 console.log(`Longest word Length ${res}`);
+
+
+
+
