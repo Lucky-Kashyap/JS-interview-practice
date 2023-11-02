06d0ec1f8ae5851946873325514f63ebbe4fc307
Lucky <luckykashyap3430@gmail.com>
2023-11-02 12:11:58 +0530

    check if an element exists in an array

--- js/array.js
+++ js/array.js
@@ -1,4 +1,3 @@
-
 // array
 
 // const myArr = [];
@@ -13,3 +12,29 @@
 
 // %DebugPrint(myArr);
 
+const arr = [1, 2, 3, 4, "hello"];
+
+// pop
+// push
+// shift
+// unshift
+
+// arr.pop();
+
+// console.log(arr);
+
+// Q1 : How do you check if an element exits in an array?
+
+const findElement = (arr, target) => {
+  for (let x of arr) {
+    console.log(x);
+    if (x == target) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(findElement(arr, "hello"));
+
+console.log(findElement(arr, 3));
