74f12ab6c788d5cabb3f86ba905b8f60717c9674
Lucky <luckykashyap3430@gmail.com>
2023-11-04 21:32:02 +0530

    start with closures

---
+++ js/closures.js
@@ -0,0 +1,16 @@
+// lexical scope
+
+// closures
+
+// var userName ='Lucky';  // global scope
+
+
+function local(){
+    // local scope
+    var userName ='Lucky'; 
+
+    
+    console.log(userName);
+}
+
+local();
