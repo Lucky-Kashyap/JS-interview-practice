bb234040ff7152470a7e1bfa38980494c9c71aa2
Lucky <luckykashyap3430@gmail.com>
2023-11-08 21:41:51 +0530

    find factorial using recursion

--- js/recursion.js
+++ js/recursion.js
@@ -6,14 +6,29 @@
 
 // trust your function
 
-function fun(n) {
-  if (n === 0) {
-    return;
-  }
-  console.log("fun", n);
-  // fun();    // maximum call stack size exceeded
+// function fun(n) {
+//   if (n === 0) {
+//     return;
+//   }
+// console.log("fun", n);
+// fun();    // maximum call stack size exceeded
 
-  fun(n - 1);
+//   fun(n - 1);
+// }
+
+// fun(10);
+
+// factorial
+
+function fact(n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
+  // return fact(n) * fact(n - 1);
 }
 
-fun(10);
+let res = fact(5);
+
+console.log(res);
