0b7dbe187a4d84839920df5cf6abc1a49e9c71fa
Lucky <luckykashyap3430@gmail.com>
2023-11-09 22:11:05 +0530

    sum object age

--- js/problem.js
+++ js/problem.js
@@ -4,24 +4,51 @@
 
 // [1,3,3,59,5,7,25,9,15,5]
 
-function convertArr(arr) {
-  let newArr = [];
+// function convertArr(arr) {
+//   let newArr = [];
 
-  // for (let i = 0; i < arr.length; i++) {
-  //   if (arr[i] % 2 == 0) {
-  //     arr[i] += 1;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     arr[i] += 1;
 
-  // newArr.push(arr[i]);
-  //   }
-  // }
+// newArr.push(arr[i]);
+//   }
+// }
 
-  newArr = arr.map((el) => (el % 2 == 0 ? (el += 1) : el));
+//   newArr = arr.map((el) => (el % 2 == 0 ? (el += 1) : el));
 
-  return newArr;
-}
+//   return newArr;
+// }
 
-const arr = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
+// const arr = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
 
-let res = convertArr(arr);
+// let res = convertArr(arr);
 
-console.log(res);
+// console.log(res);
+
+// question 2
+
+const arr = [
+  {
+    name: "jay",
+    age: 60,
+  },
+  {
+    name: "Gloria",
+    age: 36,
+  },
+  {
+    name: "Maddy",
+    age: 16,
+  },
+  {
+    name: "Joe",
+    age: 9,
+  },
+];
+
+// ouput  121 // 60 + 36 + 16 + 9
+
+// console.log(arr.map((el) => (el.age += el.age)));
+
+console.log(arr.map((el) => el.age).reduce((acc, curr) => (acc += curr)));
