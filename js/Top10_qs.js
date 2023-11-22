95ff3d04174b1a4da056dadeb0bf622e8d035032
Lucky <luckykashyap3430@gmail.com>
2023-11-22 11:27:46 +0530

    using set remove duplicates

--- js/Top10_qs.js
+++ js/Top10_qs.js
@@ -73,15 +73,17 @@ function removeDuplicates(arr) {
   //   }
   // }
 
-  let uniqueElements = [];
+  // let uniqueElements = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (uniqueElements.indexOf(arr[i]) === -1) {
-      uniqueElements.push(arr[i]);
-    }
-  }
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (uniqueElements.indexOf(arr[i]) === -1) {
+  //     uniqueElements.push(arr[i]);
+  //   }
+  // }
 
-  return uniqueElements;
+  // return uniqueElements;
+
+  return [...new Set(arr)];
 }
 
 let arr = [1, 2, 3, 4, 4, 5, 6, 6];
