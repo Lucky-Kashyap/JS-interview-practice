451b3c33bef360f32a5d486834de3355d07cf34d
Lucky <luckykashyap3430@gmail.com>
2023-11-22 11:04:10 +0530

    find longest word in the sentence

--- js/Top10_qs.js
+++ js/Top10_qs.js
@@ -1,22 +1,46 @@
 // Q1. Write a function that returns the reverse of string?
 
-function reverseString(str) {
-  // let rev = "";
+// function reverseString(str) {
+// let rev = "";
 
-  // for (let i = str.length - 1; i >= 0; i--) {
-  //   rev += str[i];
-  // }
+// for (let i = str.length - 1; i >= 0; i--) {
+//   rev += str[i];
+// }
 
-  return str.split("").reverse().join("");
+//   return str.split("").reverse().join("");
+// }
+
+// let str = "Lucky Kashyap";
+
+// let res = reverseString(str);
+
+// console.log(res);
+
+// Q2. Write a function that returns the longest word in the sentence?
+
+function longestWord(str) {
+  // let word = "";
+
+  let brea = str.split(" ");
+  let max = "";
+
+  // console.log(brea);
+
+  for (let i = 0; i < brea.length; i++) {
+    if (max.length < brea[i].length) {
+      max = brea[i];
+    }
+  }
+
+  return max;
 }
 
-let str = "Lucky Kashyap";
+let str = "I, am a React JS Developer having 3 years of experience...";
 
-let res = reverseString(str);
+let res = longestWord(str);
 
 console.log(res);
 
-// Q2. Write a function that returns the longest word in the sentence?
 // Q3. Write a function htat checks whether a given string is a palindrome or not?
 // Q4. Write a function to remove duplicate elements from an array?
 // Q5. Write a function that checks whether two strings are anagrams or not?
