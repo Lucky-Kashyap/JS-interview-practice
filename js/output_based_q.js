78b9d57ce53e634655b08324eb0bac95f95b3144
Lucky <luckykashyap3430@gmail.com>
2023-11-28 22:03:03 +0530

    question based on TDZ

--- js/output_based_q.js
+++ js/output_based_q.js
@@ -1,9 +1,28 @@
 // output based questions
 
+// Javascript Execution Context
+// Global Execution Context
+// Javascript Hoisting
+// Javascript Temporal Dead Zone
+// Javascript IIFE
+
 // 1)
+
+// console.log("Value of Age is", age);
+
+// var age = 20;
+
+// console.log("Value of Age is", age);
+
+// 2)
+
+age = 100;
 
 console.log("Value of Age is", age);
 
 var age = 20;
+
+// let age = 30;
+// age = 100;
 
 console.log("Value of Age is", age);
