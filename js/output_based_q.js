f666dd6881b3ccd6b763c6f05fc2cdf3523b947f
Lucky <luckykashyap3430@gmail.com>
2023-11-28 22:20:12 +0530

    IIFE based question

--- js/output_based_q.js
+++ js/output_based_q.js
@@ -72,3 +72,25 @@
 // var foo = 100;
 
 // console.log("FOO", foo);
+
+// 6.)
+
+variable = 10;
+
+(() => {
+  foo = 100;
+  console.log(variable);
+
+  var foo = 100;
+  variable = 20;
+
+  console.log(variable);
+})();
+
+// console.log(foo);
+
+console.log(variable);
+
+var variable = 30;
+
+console.log(variable);
