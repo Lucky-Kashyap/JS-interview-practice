515da7f55c5918e3b18156c57479a34695560091
Lucky <luckykashyap3430@gmail.com>
2023-11-28 22:29:47 +0530

    setTimeout variable scope

--- js/output_based_q.js
+++ js/output_based_q.js
@@ -75,22 +75,32 @@
 
 // 6.)
 
-variable = 10;
+// variable = 10;
 
-(() => {
-  foo = 100;
-  console.log(variable);
+// (() => {
+//   foo = 100;
+//   console.log(variable);
 
-  var foo = 100;
-  variable = 20;
+//   var foo = 100;
+//   variable = 20;
 
-  console.log(variable);
-})();
+//   console.log(variable);
+// })();
 
 // console.log(foo);
 
-console.log(variable);
+// console.log(variable);
 
-var variable = 30;
+// var variable = 30;
 
-console.log(variable);
+// console.log(variable);
+
+// 7.)
+
+// for (var i = 0; i < 10; i++) {
+//   setTimeout(() => console.log(i), 0);
+// }
+
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => console.log(i), 0);
+}
