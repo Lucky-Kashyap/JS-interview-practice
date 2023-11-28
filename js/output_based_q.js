407fdf9c63b0dd28c8b5dd653141e598e68fe637
Lucky <luckykashyap3430@gmail.com>
2023-11-28 22:10:54 +0530

    question based on function hoisting

--- js/output_based_q.js
+++ js/output_based_q.js
@@ -16,13 +16,31 @@
 
 // 2)
 
-age = 100;
+// age = 100;
 
-console.log("Value of Age is", age);
+// console.log("Value of Age is", age);
 
-var age = 20;
+// age = 100;
+
+// var age = 20;
 
 // let age = 30;
 // age = 100;
 
-console.log("Value of Age is", age);
+// console.log("Value of Age is", age);
+
+// 3)
+
+myFun();
+
+var myFun = function () {
+  console.log("First");
+};
+
+myFun();
+
+function myFun() {
+  console.log("SEcond");
+}
+
+myFun();
