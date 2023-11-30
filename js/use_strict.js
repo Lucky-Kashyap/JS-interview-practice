7c5678713322bc0b7f02a682062e8a62d0093f6a
Lucky <luckykashyap3430@gmail.com>
2023-11-30 21:55:45 +0530

    put use strict to remove common errors & warnings

---
+++ js/use_strict.js
@@ -0,0 +1,48 @@
+// use strict
+
+// varible declaration
+
+// can't delete the variable
+
+// can't make same parameters in functions
+
+// var a = 12;
+
+// console.log(a);
+
+// "use strict";
+
+// a = 12;
+
+// console.log(a);
+
+// "use strict";
+
+// var a = 12;
+
+// a = 20;
+
+// delete a;
+
+// console.log(a);
+
+// b ki value overwrite ho rhi hai
+
+// function calculator(a, b, b) {
+
+//   return a + b + b;
+// }
+
+// let res = calculator(12, 2, 3);
+
+// console.log(res);
+
+// "use strict";
+
+// function calculator(a, b, b) {
+//   return a + b;
+// }
+
+// let res = calculator(12, 2, 3);
+
+// console.log(res);
