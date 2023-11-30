05a4c4bd095acf4e576cd6754d99cc82d7a80703
Lucky <luckykashyap3430@gmail.com>
2023-11-30 18:53:28 +0530

    sort array of object

--- js/question_by_sheryians.js
+++ js/question_by_sheryians.js
@@ -1,43 +1,41 @@
 // make an array of objects representing items, filter the items based on a specific condition (e.g., items with a certain property value) and transform the filtered data into a new format.
 
-// Given an array of objects with a numerical property, implement a function to sort the objects based on that proprty. Additionally, add a order to each object based on its position in the sorted array
-
 // There are two arrays on is of users and other is of cities they live in, make a new array which contains the unified data.
 
 // 1)
 
-const products = [
-  {
-    id: 1,
-    name: "Laptop",
-    price: 999,
-    inStock: true,
-  },
-  {
-    id: 2,
-    name: "Smartphone",
-    price: 399,
-    inStock: false,
-  },
-  {
-    id: 3,
-    name: "Tablet",
-    price: 199,
-    inStock: true,
-  },
-  {
-    id: 4,
-    name: "Headphones",
-    price: 99,
-    inStock: false,
-  },
-  {
-    id: 5,
-    name: "Camera",
-    price: 599,
-    inStock: true,
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: "Laptop",
+//     price: 999,
+//     inStock: true,
+//   },
+//   {
+//     id: 2,
+//     name: "Smartphone",
+//     price: 399,
+//     inStock: false,
+//   },
+//   {
+//     id: 3,
+//     name: "Tablet",
+//     price: 199,
+//     inStock: true,
+//   },
+//   {
+//     id: 4,
+//     name: "Headphones",
+//     price: 99,
+//     inStock: false,
+//   },
+//   {
+//     id: 5,
+//     name: "Camera",
+//     price: 599,
+//     inStock: true,
+//   },
+// ];
 
 // const inStock = [];
 
@@ -51,8 +49,43 @@ const products = [
 
 // console.log(products.filter((prod) => prod.inStock === true));
 
-const ans = products
-  .filter((prod) => prod.inStock === true)
-  .map((item) => ({ name: item.name, price: item.price }));
+// const ans = products
+//   .filter((prod) => prod.inStock === true)
+//   .map((item) => ({ name: item.name, price: item.price }));
 
-console.log(ans);
+// console.log(ans);
+
+// Given an array of objects with a numerical property, implement a function to sort the objects based on that proprty. Additionally, add a order to each object based on its position in the sorted array
+
+const prices = [
+  {
+    id: 1,
+    value: 30,
+  },
+  {
+    id: 2,
+    value: 10,
+  },
+  {
+    id: 3,
+    value: 50,
+  },
+  {
+    id: 4,
+    value: 20,
+  },
+  {
+    id: 5,
+    value: 40,
+  },
+];
+
+for (let i = 0; i < prices.length - 1; i++) {
+  for (let j = i + 1; j < prices.length; j++) {
+    if (prices[i].value > prices[j].value) {
+      const temp = prices[i];
+      prices[i] = prices[j];
+      prices[j] = temp;
+    }
+  }
+}
