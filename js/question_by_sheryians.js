8cf40eeed9b7456a69c1a5efcfd269f7ddd55e13
Lucky <luckykashyap3430@gmail.com>
2023-11-30 19:09:58 +0530

    find unified data and add property to it

--- js/question_by_sheryians.js
+++ js/question_by_sheryians.js
@@ -1,7 +1,5 @@
 // make an array of objects representing items, filter the items based on a specific condition (e.g., items with a certain property value) and transform the filtered data into a new format.
 
-// There are two arrays on is of users and other is of cities they live in, make a new array which contains the unified data.
-
 // 1)
 
 // const products = [
@@ -57,28 +55,28 @@
 
 // Given an array of objects with a numerical property, implement a function to sort the objects based on that proprty. Additionally, add a order to each object based on its position in the sorted array
 
-const prices = [
-  {
-    id: 1,
-    value: 30,
-  },
-  {
-    id: 2,
-    value: 10,
-  },
-  {
-    id: 3,
-    value: 50,
-  },
-  {
-    id: 4,
-    value: 20,
-  },
-  {
-    id: 5,
-    value: 40,
-  },
-];
+// const prices = [
+//   {
+//     id: 1,
+//     value: 30,
+//   },
+//   {
+//     id: 2,
+//     value: 10,
+//   },
+//   {
+//     id: 3,
+//     value: 50,
+//   },
+//   {
+//     id: 4,
+//     value: 20,
+//   },
+//   {
+//     id: 5,
+//     value: 40,
+//   },
+// ];
 
 // for (let i = 0; i < prices.length - 1; i++) {
 //   for (let j = i + 1; j < prices.length; j++) {
@@ -96,8 +94,44 @@ const prices = [
 //   .sort((a, b) => a.value - b.value)
 //   .map((item) => ({ ...item, grade: "very well" }));
 
-const data = prices
-  .sort((a, b) => a.value - b.value)
-  .map((item, index) => ({ ...item, grade: index + 1 }));
+// const data = prices
+//   .sort((a, b) => a.value - b.value)
+//   .map((item, index) => ({ ...item, grade: index + 1 }));
+
+// console.log(data);
+
+// There are two arrays on is of users and other is of cities they live in, make a new array which contains the unified data.
+
+const users = [
+  {
+    id: 1,
+    name: "John",
+    age: 24,
+  },
+  {
+    id: 2,
+    name: "Johnny",
+    age: 29,
+  },
+  {
+    id: 3,
+    name: "John ke Chacha",
+    age: 21,
+  },
+];
+
+const cities = [
+  {
+    id: 11,
+    city: "Agra",
+  },
+  { id: 2, city: "Delhi" },
+  { id: 3, city: "Bijnor" },
+];
+
+const data = users.map((item) => ({
+  ...item,
+  ...cities.find((item2) => item.id === item2.id),
+}));
 
 console.log(data);
