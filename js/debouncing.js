e00151f16b66fcfd62fa9ebde73d322b7c1d8ce9
Lucky <luckykashyap3430@gmail.com>
2023-12-25 14:23:53 +0530

    constructor function

--- js/debouncing.js
+++ js/debouncing.js
@@ -34,12 +34,46 @@
 
 // eval () is a built-in function that evaluates a string as a JavaScript code and dynamically executes it.
 
-let num = 10;
+// let num = 10;
 
-let num2 = 20;
+// let num2 = 20;
 
-let res = "num + num2";
+// let res = "num + num2";
 
-console.log(eval(res));
+// console.log(eval(res));
 
-console.log(typeof eval("120"));
+// console.log(typeof eval("120"));
+
+// Waht are constructor functions?
+
+// constructor functions are a way of creating objects and initializing their properties
+
+// using class
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    console.log(`${this.name} + ${this.age}`);
+  }
+}
+
+// constructor function
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Creating objects from class
+
+var person1 = new Person("Bob", 23);
+
+//  uisng constructor function
+
+// same way object create
+
+var person2 = new Person("Ajay", 26);
