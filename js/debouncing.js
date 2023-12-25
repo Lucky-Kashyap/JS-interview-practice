168029b5f986ca37ff52e7386f2bb012a88c942c
Lucky <luckykashyap3430@gmail.com>
2023-12-25 14:07:33 +0530

    eval method

--- js/debouncing.js
+++ js/debouncing.js
@@ -9,23 +9,37 @@
 
 // function baar baar call na ho
 
-let input = document.getElementById("search");
+// let input = document.getElementById("search");
 
-let count = 0;
+// let count = 0;
 
-function getData() {
-  count++;
-  console.log("fetching data", count);
-}
+// function getData() {
+//   count++;
+//   console.log("fetching data", count);
+// }
 
-function myDebounce(call, id) {
-  let timer;
+// function myDebounce(call, id) {
+//   let timer;
 
-  return function (...args) {
-    if (timer) clearTimeout(timer);
-    setTimeout(() => {
-      call();
-    }, id);
-  };
-}
-const betterFunction = myDebounce(getData, 1000);
+//   return function (...args) {
+//     if (timer) clearTimeout(timer);
+//     setTimeout(() => {
+//       call();
+//     }, id);
+//   };
+// }
+// const betterFunction = myDebounce(getData, 1000);
+
+// What is eval() function in JS ?
+
+// eval () is a built-in function that evaluates a string as a JavaScript code and dynamically executes it.
+
+let num = 10;
+
+let num2 = 20;
+
+let res = "num + num2";
+
+console.log(eval(res));
+
+console.log(typeof eval("120"));
