af28e2922ff130072a772af300589ff065ace43a
Lucky <luckykashyap3430@gmail.com>
2024-01-04 16:44:03 +0530

    practical usecase of IIFE

--- js/iffe_func.js
+++ js/iffe_func.js
@@ -7,3 +7,21 @@
 // })();
 
 // a();
+
+// practical  use case
+
+// it will not polute the global scope
+
+(function () {
+  const h1 = document.querySelector("h1");
+
+  const p = document.querySelector("p");
+
+  const num = 120;
+
+  h1.style.backgroundColor = "pink";
+
+  h1.style.color = "black";
+
+  p.innerText = num;
+})();
