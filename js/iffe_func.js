34fb026755ede48c044435947376da2e9bcc2726
Lucky <luckykashyap3430@gmail.com>
2024-01-04 16:54:45 +0530

    another syntax for IIFE

--- js/iffe_func.js
+++ js/iffe_func.js
@@ -12,6 +12,20 @@
 
 // it will not polute the global scope
 
+// (function () {
+//   const h1 = document.querySelector("h1");
+
+//   const p = document.querySelector("p");
+
+//   const num = 120;
+
+//   h1.style.backgroundColor = "pink";
+
+//   h1.style.color = "black";
+
+//   p.innerText = num;
+// })();
+
 (function () {
   const h1 = document.querySelector("h1");
 
