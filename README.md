88aa261e1a498c1c0fa83ba20be6db84a963a212
Lucky-Kashyap <luckykashyap3430@gmail.com>
2024-04-19 17:02:47 +0530

    day 8

--- README.md
+++ README.md
@@ -1555,6 +1555,23 @@ Output: Violet-Indigo-Blue-Green-Yellow-Orange-Red
 
 #### Write a function findMax that takes an array of numbers as input nd returns the maximum number in the array.
 
+## Day 8
+
+### Challenge: Factorial Finder
+
+#### Write a function factorial that takes a non-negative integer num as input and returns its factorial. The factorial of a non-negative integer n, denoted as n!, is the product of all positive integers less than or equal to n. The factorial of 0 is defined as 1.
+
+- Here are some examples of factorial calculations:
+
+   - factorial(0) => 1
+   - factorial(1) => 1
+   - factorial(2) => 2
+   - factorial(3) => 6
+   - factorial(4) => 24
+   - factorial(5) => 120
+
+- Your function should work for any non-negative integer
+
 # Interview Prep
 
 Happy Learning! ✨
--- js/thapa/q1.js
+++ js/thapa/q1.js
@@ -271,19 +271,60 @@
 
 // Example Usage:
 
-const findMax = (arr) => {
+// const findMax = (arr) => {
   // return Math.max(...arr);
 
-  return arr.reduce((acc, elem) => {
-    if (acc < elem) {
-      acc = elem;
-    }
-    return acc;
-  });
-};
+//   return arr.reduce((acc, elem) => {
+//     if (acc < elem) {
+//       acc = elem;
+//     }
+//     return acc;
+//   });
+// };
 
-console.log(findMax([1, 5, 3, 9, 2])); // Output: 9
+// console.log(findMax([1, 5, 3, 9, 2])); // Output: 9
 
-console.log(findMax([-10, -5, -3, -9, -2])); // Output: -2
+// console.log(findMax([-10, -5, -3, -9, -2])); // Output: -2
 
-console.log(findMax([5])); // Output: 5
+// console.log(findMax([5])); // Output: 5
+
+
+//  #Day 8
+
+// Challenge: Factorial Finder
+
+// Write a function factorial that takes a non-negative integer num as input and returns its factorial. The factorial of a non-negative integer n, denoted as n!, is the product of all positive integers less than or equal to n. The factorial of 0 is defined as 1.
+
+// Here are some examples of factorial calculations:
+
+// factorial(0) => 1
+// factorial(1) => 1
+
+// factorial(2) => 2
+
+// factorial(3) => 6
+
+// factorial(4) => 24
+
+// factorial(5) => 120
+
+
+// Your function should work for any non-negative integer
+
+const factorial= (num)=>{
+  let fact = 1;
+
+  for(let i=1;i<=num;i++){
+    fact *=i;
+  }
+
+  return fact;
+}
+
+
+console.log(factorial(5));  // Output: 120
+
+console.log(factorial(0));   // Output: 1
+
+console.log(factorial(3));   // Output: 6
+
