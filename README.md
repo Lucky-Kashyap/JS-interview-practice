1dba648f6d5f29a86b89387355104c48a6818e43
Lucky <luckykashyap3430@gmail.com>
2024-05-19 21:01:25 +0530

    day 31

--- README.md
+++ README.md
@@ -1835,6 +1835,27 @@ Output: Violet-Indigo-Blue-Green-Yellow-Orange-Red
 
 ## Day 31
 
+#### Write a function called simplePasswordValidator that takes a single parameter
+
+- password: A string representing the password to be validated.
+  The function should validate the password based on following criteria:
+
+- The password must contain at least one lowercase letter, one uppercase letter, and one digit
+  The length of the password must be at least 8 characters
+  The function should return true if the password meets all the criteria, otherwise, it should return false
+
+- Input:
+
+  - password: A non-empty string representing the password to be validated.
+
+- Output:
+
+  - true if the password meets all the criteria
+  - false otherwise
+
+- Constraints
+  - The input string password will contain only alphanumeric characters and punctuation marks
+
 ####
 
 # Interview Prep
--- js/thapa/q1.js
+++ js/thapa/q1.js
@@ -1049,6 +1049,7 @@
 // };
 
 // console.log(repeatString("abc", 10));
+// console.log(repeatString("lucky", 5));
 
 // Output: 'abcabcabcabcabc'
 
@@ -1154,3 +1155,49 @@
 // console.log(numberRangeRecursive(-2, 2)); // Output: [-2,-1,0,1,2]
 
 // Day 31
+
+// Write a function called simplePasswordValidator that takes a single parameter
+
+// password: A string representing the password to be validated.
+// The function should validate the password based on following criteria:
+
+// The password must contain at least one lowercase letter, one uppercase letter, and one digit
+// The length of the password must be at least 8 characters
+// The function should return true if the password meets all the criteria, otherwise, it should return false
+
+// Input:
+// password: A non-empty string representing the password to be validated.
+
+// Output:
+// true if the password meets all the criteria
+// false otherwise
+
+// Constraints
+// The input string password will contain only alphanumeric characters and punctuation marks
+
+const simplePasswordValidator = (str) => {
+  let lowerCasePass = false;
+  let upperCasePass = false;
+  let numberPass = false;
+
+  for (let elem of str) {
+    if (elem.charCodeAt(0) >= 65 && elem.charCodeAt(0) <= 90) {
+      upperCasePass = true;
+    } else if (elem.charCodeAt(0) >= 97 && elem.charCodeAt(0) <= 122) {
+      lowerCasePass = true;
+    } else if (!isNaN(Number(elem))) {
+      numberPass = true;
+    }
+  }
+
+  if (!lowerCasePass || !upperCasePass || !numberPass || str.length < 8) {
+    return false;
+  }
+  return true;
+};
+
+console.log(simplePasswordValidator("avhLK234jhb"));
+
+console.log(simplePasswordValidator("avhhjbL4"));
+console.log(simplePasswordValidator("2Dgvhhjbjhj"));
+console.log(simplePasswordValidator("avhhjbjhj"));
